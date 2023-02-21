7ea4bd8050a9efb0ba4adb2f21de67aeb1b4055b
VasilyeuViacheslau <106859411+atomiccf@users.noreply.github.com>
2023-02-21 21:26:18 +0300

    add  ai style  improvements

--- ai/ai.js
+++ ai/ai.js
@@ -2,7 +2,7 @@ let field = document.getElementById('field');
 let messageOne = document.getElementById('firstPlayer_gameField');
 let messageTwo = document.getElementById('secondPlayer_gameField');
 let nameOne = document.getElementById('firstPlayer_name');
-let nameTwo = document.getElementById('secondPlayer_name');
+
 let pointsOne = document.getElementById('firstPlayer_info');
 
 let btn = document.getElementById('move');
@@ -11,30 +11,26 @@ const langRegex = /^[A-Za-z]+$/
 const clickAudio = new Audio("../src/pencil_write.mp3");
 let cities = [];
 
-let turn = true;
-let gameOver = true;
-
 let pW = new Player();
-let pS = new Player();
 
-const rand = (a, b) => Math.floor((Math.random() * (b - a + 1) + a));
+
+reg ()
 
 btn.addEventListener('click', (ev) => {
 
     if (field.value === '' || field.value.match(langRegex)) {
         ev.preventDefault();
 
-    } else if (turn) {
+    } else  {
 
         pW.render(pW.move(), messageOne, 'bubble')
-
-        pW.message(`Ходит компьютер`)
+        clickSound()
 
         pointsOne.innerText = `Score ${pW.points}`;
 
-        setTimeout(aiTurn, rand(1000,10000));
+        setTimeout(aiTurn,  rand(1000,10000));
 
-        pW.message(pW.name);
+        pW.message('Ход компьютера');
 
     }
     //   turn = !turn;
@@ -42,13 +38,9 @@ btn.addEventListener('click', (ev) => {
 })
 
 
+
 function aiTurn() {
-    /*if (cities.length === 0) {
-        console.log(0);
-        let aiCity = randomCity()
-        cities.push(aiCity)
-        renderAl(aiCity, messageTwo, 'bubble_second');
-    }*/
+
 
     let aiCity = randomCity()
     const lastCity = cities[cities.length - 1];
@@ -94,3 +86,31 @@ function clickSound() {
     clickAudio.currentTime = 0; // в секундах
     clickAudio.play();
 }
+
+function reg (){
+    const regPlayer = document.getElementById('reg');
+    regPlayer.classList.remove('reg')
+    regPlayer.classList.add('registration_show');
+    const reg_valueOne = document.getElementById('reg_valueOne')
+    const reg_button = document.getElementById('reg_button');
+
+    reg_button.addEventListener('click', (ev) => {
+        if (reg_valueOne.value === '' ) {
+            ev.preventDefault();
+
+        } else {
+            regPlayer.classList.remove('registration_show');
+            regPlayer.classList.add('registration_hide');
+            regPlayer.classList.remove('registration_hide');
+            regPlayer.classList.add('reg')
+            pW.name = reg_valueOne.value;
+            nameOne.innerText = `Player: ${pW.name}`
+            pointsOne.innerText = `Score ${pW.points}`
+
+        }
+        pW.message(`Ходит ${pW.name}`)
+    });
+
+}
+
+const rand = (a, b) => Math.floor((Math.random() * (b - a + 1) + a));
